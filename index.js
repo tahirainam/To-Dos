1ef85a146ec867136e99d38bd5dfe046a4e73fcd
inahira <sololearner120@gmail.com>
2023-10-31 02:41:37 -0700

    added basic structure & logic

---
+++ index.js
@@ -0,0 +1,16 @@
+//javaScript logic for To-Do list will go here
+function addTask() {
+    var taskInput = document.getElementById("taskInput");
+    var taskList = document.getElementById("taskList");
+
+    if (taskInput.value.trim() !== "") {
+        var li = document.createElement("li");
+        li.innerHTML = `<span>${taskInput.value}</span> <span class="delete" onclick="deleteTask(this)">❌</span>`;
+        taskList.appendChild(li);
+        taskInput.value = "";
+    }
+}
+
+function deleteTask(element) {
+    element.parentElement.remove();
+}
